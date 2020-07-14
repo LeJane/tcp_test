da837880eb43121f3b477cf71b2a0071f19e55ad
lanshibao <lanshibao@localhost.localdomain>
2020-07-14 12:52:45 +0800

    first

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use byteorder::{LittleEndian, ReadBytesExt};
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let mut buf = [0; 1024];
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                let mut cursor = std::io::Cursor::new(&buf[..n]);
+                let x = ReadBytesExt::read_i32::<LittleEndian>(&mut cursor).unwrap();
+                println!("xx:{:?}", x);
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
